6c065118259bdfb9a0e2ac253ccdbed39971c22a
zodiacVG <zhangzibo418@outlook.com>
2021-05-09 14:25:31 +0800

    Revert "完成选择问答问题数量界面"
    This reverts commit fd09fb71ec2d0d29fb958560ec76ff456dd0e285.

--- miniprogram/pages/uploadQuestionFileReadyPage/uploadQuestionFileReadyPage.js
+++ miniprogram/pages/uploadQuestionFileReadyPage/uploadQuestionFileReadyPage.js
@@ -5,8 +5,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-    fileSource:'', //文件路径
-    fileType:'',  //文件类型
     totalQuestionsNum:5, //问题总数
     needQuestionsNum:3  //需要答对的问题数
   },
@@ -15,32 +13,17 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      fileSource:options.filePath,
-      fileType:options.fileType
-    })
-  },
 
-  onDragTotalQuestionsNum(e){ //拖拽进度条
+  },
+  onDragTotalQuestionsNum(e){
     this.setData({
       totalQuestionsNum:e.detail.value
-    })
-    this.selectComponent('#needQuestionsNumSlider').init({
-      ...this.data.data
     })
   },
 
   onDragNeedQuestionsNum(e){
     this.setData({
       needQuestionsNum:e.detail.value
-    })
-  },
-
-  startSelectQuestion(){ //开始选择问题,跳转界面
-    wx.navigateTo({
-      url: '../selectQuestionsPage/selectQuestionsPage?filePath='+this.data.fileSource+
-      '&fileType='+this.data.fileType+'&totalQuestionsNum='+this.data.totalQuestionsNum+
-      '&needQuestionsNum='+this.data.needQuestionsNum
     })
   }
 
